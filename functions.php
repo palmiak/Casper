d4a5593cd485d209815d37be7b0bcaa5721d3049
Maciej Palmowski <maciek@insanelab.com>
2016-08-24 21:27:56 +0200

    TGM ACF ready

--- functions.php
+++ functions.php
@@ -16,6 +16,8 @@ $sage_includes = [
 	'lib/titles.php',    // Page titles
 	'lib/wrapper.php',   // Theme wrapper class
 	'lib/customizer.php', // Theme customizer
+	'lib/class-tgm-plugin-activation.php', //TGM
+	'lib/acf.php', //acf fields
 ];
 
 foreach ( $sage_includes as $file ) {
@@ -59,3 +61,53 @@ function unregister_categories() {
 	unregister_taxonomy_for_object_type( 'category', 'post' );
 }
 add_action( 'init', 'unregister_categories' );
+
+add_action( 'tgmpa_register', 'sage_register_required_plugins' );
+
+function sage_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		array(
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+			'is_callable' => 'wpseo_init',
+		),
+		array(
+			'name'        => 'Disqus',
+			'slug'        => 'disqus-comment-system',
+			'is_callable' => 'disqus_init',
+		),
+		array(
+			'name'        => 'Advanced Custom Fields',
+			'slug'        => 'advanced-custom-fields',
+			'is_callable' => 'acf_init',
+		),
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'sage',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
